8c592cf65abb4d6b9a84a7bebdfa250c401cc3a9
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-06-27 14:57:18 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 3.97 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public Object solution(String expression) {
+        String[][] operator = {
+            {"\\+","\\-","\\*"},{"\\+","\\*","\\-"},{"\\-","\\+","\\*"},
+            {"\\-","\\*","\\+"},{"\\*","\\+","\\-"},{"\\*","\\-","\\+"}
+        };
+        
+        long answer = 0;
+        
+        for ( String[] ope : operator ) {
+            System.out.printf("\n%c  %c  %c\n", ope[0].charAt(1), ope[1].charAt(1), ope[2].charAt(1));
+            
+            answer = Math.max(answer, Math.abs(calc(expression, ope, 0)));
+        }
+        return answer;
+    }
+    
+    long calc(String expression, String[] operator, int k) {
+        if ( k==3 || !( expression.contains("+") || expression.contains("-") || expression.contains("*") ) )
+            return Long.parseLong(expression);
+
+        ArrayList<Long> list = new ArrayList<>();
+
+        for ( String exp : expression.split(operator[k]) ) list.add(calc(exp, operator, k + 1));
+    
+        Long result = list.get(0);
+        
+        for ( int i = 1 ; i < list.size() ; i++ ) result = operating(result, list.get(i), operator[k]);    
+        
+        return result;
+    }
+    
+    long operating(Long a, Long b, String ope) {
+        switch ( ope.charAt(1) ) {
+            case '+' : return a + b;
+            case '-' : return a - b;
+            default : return a * b;
+        }
+    }
+}

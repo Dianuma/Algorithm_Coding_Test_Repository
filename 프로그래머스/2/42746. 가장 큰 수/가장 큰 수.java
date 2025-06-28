85c2c2fb102eff4bb7f475a5cf1647a64d9f2d88
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-06-28 11:49:47 +0900

    [level 2] Title: 가장 큰 수, Time: 82.30 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < numbers.length; i++) {
-            list.add(numbers[i]);
+        List<int[]> intermediateSalt = new ArrayList<>();
+        for ( int i = 0 ; i < numbers.length ; i++ ) {
+            // 배열의 숫자를 4회 ( numbers 원소는 최대 1000 ) 반복 후 4자리까지 끊어서 인덱스와 함께 저장
+            // ex - [2, 23, 221] => [[2222, 0], [2323, 1], [2212, 2]]
+            String strInt = Integer.toString(numbers[i]).repeat(4).substring(0, 4);
+            intermediateSalt.add(new int[]{Integer.parseInt(strInt),i});
         }
-        Collections.sort(list, (a, b) -> {
-            String as = String.valueOf(a), bs = String.valueOf(b);
-            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
-        });
-        StringBuilder sb = new StringBuilder();
-        for(Integer i : list) {
-            sb.append(i);
-        }
-        answer = sb.toString();
-        if(answer.charAt(0) == '0') {
-            return "0";
-        }else {
-            return answer;
-        }
+        
+        // 첫번째 원소를 기준으로 리스트를 내림차순 정렬
+        // ex - [[2323, 1], [2222, 0], [2212, 2]]
+        intermediateSalt.sort((a, b) -> Integer.compare(b[0], a[0]));
+        StringBuilder answer = new StringBuilder();
+        
+        // answer 에 저장 후 answer 의 첫 글자가 0 -> "0" 반환 / 아니면 answer 반환
+        for ( int[] salted : intermediateSalt )
+            answer.append(Integer.toString(numbers[salted[1]]));
+        
+        return (answer.charAt(0) == '0') ? "0" : answer.toString();
     }
 }

2c662512c4e4c8d5998af939a9ffb0edf580f7d9
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-07-08 11:38:05 +0900

    [level 3] Title: 억억단을 외우자, Time: 442.58 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,42 +1,17 @@
-import java.util.*;
-
 class Solution {
-    public Object solution(int e, int[] starts) {
-        int[] answer = new int[starts.length];
-        int[][] map = new int[e][2];
-        int count = 0;
-        for ( int i = 1 ; i <= e ; i++ ) {
-            map[i - 1][0] = i;
-            for ( int j = 1 ; j <= e / i ; j++ ) {
-                if ( i * j > e ) continue; 
-                map[i*j - 1][1]++;
-                count++;
-            }
-        }
-    
-        Arrays.sort(map, new Comparator<int[]>() {
-
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                int answer = o2[1] - o1[1];
-                if (answer == 0) {
-                    answer = o1[0] - o2[0];
-                }
-                return answer;
-            }
-        });
-
-        for (int i = 0 ; i < starts.length ; i++) {
-            int min = starts[i];
-
-            for (int j = 0 ; j < map.length ; j++) {
-                if (map[j][0] >= min) {
-                    answer[i] = map[j][0];
-                    break;
-                }
-            }
-
-        }
+    public Object solution(int e, int[] starts) {        
+        int[] answer = new int[starts.length], map = new int[e + 1], max = new int[e + 1];
+        
+        max[e] = e;
+        
+        for ( int i = 1 ; i <= e ; i++ ) 
+            for ( int j = 1 ; j <= e / i ; j++ )  map[i * j]++;
+        
+        for ( int i = e - 1 ; i >= 0 ; i-- ) 
+            if ( map[i] >= map[max[i + 1]] ) max[i] = i;
+            else max[i] = max[i + 1];
+        
+        for ( int i = 0 ; i < starts.length ; i++ ) answer[i] = max[starts[i]];
         
         return answer;
     }

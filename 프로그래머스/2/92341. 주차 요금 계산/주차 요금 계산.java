80ecd2f85a096fa9cffe52441219f68d70de573a
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-10-02 23:41:14 +0900

    [level 2] Title: 주차 요금 계산, Time: 10.65 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,41 +1,21 @@
 import java.util.*;
-import java.time.*;
-
 class Solution {
     public Object solution(int[] fees, String[] records) {
-        int[] answer = {};
-        int i = 0;
-        Map<String, String> carMap = new HashMap<>();
         TreeMap<String, Integer> timeMap = new TreeMap<>();
-        
-        for ( String r : records ) timeMap.put(r.split(" ")[1], 0);
-        
         for ( String r : records ) {
             String[] data = r.split(" ");
-            if ( data[2].equals("IN") ) carMap.put(data[1], data[0]);
-            else {
-                calcTime(timeMap, data[1], carMap.get(data[1]), data[0]);
-                carMap.remove(data[1]);
-            }
+            int diff = data[2].equals("IN") ? -timeToInt(data[0]) : timeToInt(data[0]);
+            timeMap.merge(data[1], diff, Integer::sum);
         }
-        
-        for ( String key : carMap.keySet() ) calcTime(timeMap, key, carMap.get(key), "23:59");
-        
-        answer = new int[timeMap.size()];
+        int idx = 0, answer[] = new int[timeMap.size()];
         for (Integer time : timeMap.values()) {
-            answer[i] = (time > fees[0]) ? fees[1] + (int) Math.ceil( (time - fees[0]) * 1.0 / fees[2] ) * fees[3]: fees[1];
-            i++;
+            if ( time <= 0 ) time += timeToInt("23:59");
+            answer[idx++] = (time > fees[0]) ? fees[1] + (int) Math.ceil( (time - fees[0]) * 1.0 / fees[2] ) * fees[3]: fees[1];
         }
-        
         return answer;
     }
-    
-    void calcTime(TreeMap<String, Integer> timeMap, String carNum, String in, String out) {
-        LocalTime inTime = LocalTime.parse(in);
-        LocalTime outTime = LocalTime.parse(out);
-        
-        int diff = (int) Duration.between(inTime, outTime).toMinutes();
-            
-        timeMap.compute(carNum, (k, v) -> v + diff);
+    int timeToInt(String time){
+        String[] parts = time.split(":");
+        return Integer.parseInt(parts[0]) * 60 + Integer.parseInt(parts[1]);
     }
 }

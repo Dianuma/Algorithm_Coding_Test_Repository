4ac6fc17dafe80e4d3d5ab98bc9a178e4ef291de
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-10-03 16:04:58 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 58.34 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,26 +1,27 @@
 class Solution {
     public Object solution(int[][] board, int[][] skill) {
         int answer = 0;
-        int[][] sum = new int[board.length + 1][board[0].length + 1];
+        int[][] damage = new int[board.length + 1][board[0].length + 1];
+        
         for ( int[] s : skill ) {
-            int degree = ( s[0] == 1 ) ? -s[5] : s[5];
-            sum[s[1]][s[2]] += degree;
-            sum[s[1]][s[4] + 1] -= degree;
-            sum[s[3] + 1][s[2]] -= degree;
-            sum[s[3] + 1][s[4] + 1] += degree;
+            int type = ( s[0] == 1 ) ? -1 : 1;
+            damage[s[1]][s[2]] += type * s[5];
+            damage[s[1]][s[4]+1] -= type * s[5];
+            damage[s[3]+1][s[2]] -= type * s[5];
+            damage[s[3]+1][s[4]+1] += type * s[5];
         }
         
-        for ( int i = 0 ; i < board.length + 1 ; i++ ) 
-            for ( int j = 1 ; j < board[0].length + 1 ; j++ ) 
-                sum[i][j] += sum[i][j - 1];
+        for ( int i = 0 ; i < damage.length ; i++ ) 
+            for ( int j = 1 ; j < damage[0].length ; j++ ) 
+                damage[i][j] += damage[i][j-1];
         
-        for ( int j = 0 ; j < board[0].length + 1 ; j++ ) 
-            for ( int i = 1 ; i < board.length + 1 ; i++ ) 
-                sum[i][j] += sum[i - 1][j];
-
-        for ( int i = 0 ; i < board.length ; i++ )
-            for ( int j = 0 ; j < board[0].length ; j++ )
-                if ( board[i][j] + sum[i][j] > 0 ) answer++;
+        for ( int i = 0 ; i < damage[0].length ; i++ ) 
+            for ( int j = 1 ; j < damage.length ; j++ ) 
+                damage[j][i] += damage[j-1][i];           
+        
+        for ( int i = 0 ; i < board.length ; i++ ) 
+            for ( int j = 0 ; j < board[0].length ; j++) 
+                if ( board[i][j] + damage[i][j] > 0 ) answer++;
         
         return answer;
     }

7740c26142d9003905c0b3a3327bb0fdde30ced5
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-10-07 21:19:28 +0900

    [level 1] Title: 모의고사, Time: 6.50 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,10 +1,7 @@
 def solution(answers):
     s = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
     a = [0, 0, 0]
-    answer = []
     for i in range(len(answers)):
         for j in range(3):
             if s[j][i%len(s[j])] == answers[i]: a[j]+=1
-    max_score = max(a)
-    answer = [i + 1 for i, score in enumerate(a) if score == max_score]
-    return answer
+    return [i + 1 for i, score in enumerate(a) if score == max(a)]

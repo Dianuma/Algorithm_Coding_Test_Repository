b4195fbd3f8c385539eb0150e678537e78fa49ae
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-10-29 21:15:39 +0900

    [level 2] Title: 미로 탈출, Time: 14.28 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,45 +1,19 @@
 from collections import deque
 
-def fast_way_bfs(maps, start, end):
-    visited = [[0] * 1000 for _ in range(1000)]
-    drs, dcs = [0, 0, 1, -1], [1, -1, 0, 0]
-    queue = deque()
-    queue.append(start)
-
-    distance = 0
-    while len(queue):
-        r, c, dist = queue.popleft()
-        if not(0 <= r < len(maps)) or not(0 <= c < len(maps[0])):
-            continue
-        if maps[r][c] == 'X' or visited[r][c] == True:
-            continue 
-        if maps[r][c] == maps[end[0]][end[1]]:
-            return dist
-        # print(queue)
-        # print(x, y, maps[x][y], dist)
-
-        visited[r][c] = True
-
-        for dr, dc in zip(drs, dcs):
-            if 0 <= r+dr < len(maps) and 0 <= c+dc < len(maps[0]):
-                if maps[r+dr][c+dc] != 'X' and visited[r+dr][c+dc] == False:
-                    queue.append((r+dr, c+dc, dist+1))
-
-    return -1
-
+def get_reach_time(maps, start, end):
+    d, q, visited = [(-1, 0), (0, -1), (1, 0), (0, 1)], deque([(*start, 0)]), set(start)
+    R, C = len(maps), len(maps[0])
+    while q:
+        r, c, time = q.popleft()
+        for dr, dc in d:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < R and 0 <= nc < C and (nr, nc) not in visited and maps[nr][nc] != "X":
+                if maps[nr][nc] == end: return time + 1
+                visited.add((nr, nc))
+                q.append((nr, nc, time + 1))
+    return 0
 
 def solution(maps):
-    start, lever, end = 0, 0, 0
-    for r in range(len(maps)):
-        for c in range(len(maps[0])):
-            if maps[r][c] == 'S':
-                start = r, c, 0
-            if maps[r][c] == 'E':
-                end = r, c, 0
-            if maps[r][c] == 'L':
-                lever = r, c, 0
-
-    first, second = fast_way_bfs(maps, start, lever), fast_way_bfs(maps, lever, end)
-    if -1 in [first, second]:
-        return -1
-    return first + second
+    visit = {maps[r][c]: (r, c) for r in range(len(maps)) for c in range(len(maps[0])) if maps[r][c] in "SEL"}
+    a, b = get_reach_time(maps, visit["S"], "L"), get_reach_time(maps, visit["L"], "E")
+    return a + b if a and b else -1

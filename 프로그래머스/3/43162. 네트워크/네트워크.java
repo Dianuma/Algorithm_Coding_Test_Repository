f2096448da0cd8bcc652f39381c1d36a0d047a19
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-10-29 21:24:54 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,21 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
-        List<int[]> mapList = new ArrayList<>();
-        int[] parent = new int[n];
-        Set<Integer> answer = new HashSet<>();
-        for (int i = 0; i < n; i++) parent[i] = i;
-        for ( int i = 0 ; i < computers.length ; i++ )
-            for ( int j = 0 ; j < computers[i].length ; j++ ) 
-                if ( i!=j && computers[i][j] == 1 ) mapList.add(new int[]{Math.min(i,j), Math.max(i,j)});
-        for (int[] map : mapList) {
-            int a = find(parent, map[0]), b = find(parent, map[1]);
-            if (a != b) parent[b] = a;   
+        int answer = 0;
+        boolean[] chk = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(!chk[i]) {
+                dfs(computers, chk, i);
+                answer++;
+            }
         }
-        for (int i = 0; i < n; i++) answer.add(find(parent, i));
-        return answer.size();
+        return answer;
     }
-    private int find(int[] parent, int x) {
-        return parent[x] == x ? x : (parent[x] = find(parent, parent[x]));
+    void dfs(int[][] computers, boolean[] chk, int start) {
+        chk[start] = true;
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[start][i] == 1 && !chk[i]) {
+                dfs(computers, chk, i);
+            }
+        }
     }
 }

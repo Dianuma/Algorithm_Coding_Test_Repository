4caff9c6a94e1241037096f8e79b5db0fa2c0416
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2025-10-30 00:26:47 +0900

    [level 3] Title: 네트워크, Time: 3.17 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,9 +1,20 @@
 def solution(n, computers):
-    groups = []
-    for s in [{r,c} for r in range(len(computers)) for c in range(r, len(computers)) if computers[r][c]]:
-        merged = []
-        for g in groups:
-            if s & g: s |= g
-            else: merged.append(g)
-        groups = merged + [s]
-    return len(groups)
+    answer = 0
+    visited = [0 for i in range(n)]
+    def dfs(computers, visited, start):
+        stack = [start]
+        while stack:
+            j = stack.pop()
+            if visited[j] == 0:
+                visited[j] = 1
+            # for i in range(len(computers)-1, -1, -1):
+            for i in range(0, len(computers)):
+                if computers[j][i] ==1 and visited[i] == 0:
+                    stack.append(i)
+    i=0
+    while 0 in visited:
+        if visited[i] ==0:
+            dfs(computers, visited, i)
+            answer +=1
+        i+=1
+    return answer

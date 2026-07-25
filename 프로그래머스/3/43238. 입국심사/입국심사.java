e01dffafbee0b1a5a7aa7a341ad15c2cc80097b8
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2026-07-25 19:40:04 +0900

    [level 3] Title: 입국심사, Time: 18.24 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,16 +2,31 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-        long left = 1, right = (long) times[times.length - 1] * n, answer = right;
-        while (left <= right) {
-            long mid = (left + right) / 2, sum = 0;
-            for (int time : times) sum += mid / time;
-            if (sum >= n) {
-                answer = mid;
-                right = mid - 1;
-            } else left = mid + 1;
+        long max = 0;
+        
+        for ( int time : times ) {
+            max = Math.max(max, time);
         }
-        return answer;
+        
+        long left = 1, right = max * n;
+        
+        while ( true ) {
+            if ( left >= right ) break;
+            
+            long mid = ( left + right ) / 2;
+            
+            long person = 0;
+            
+            for ( int time : times ) {
+                person += mid / time;
+            }
+            
+            if ( person < n ) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
     }
 }

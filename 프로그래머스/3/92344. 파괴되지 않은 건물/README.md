0f8dffe18805e46f8291209f86e81f137128ec9c
LEE Seunghyun <83849667+Dianuma@users.noreply.github.com>
2026-07-25 19:46:58 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 30.81 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
@@ -1,10 +1,10 @@
 # [level 3] 파괴되지 않은 건물 - 92344 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 212 MB, 시간: 58.34 ms
+메모리: 184 MB, 시간: 30.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 03일 16:04:56
+2026년 07월 25일 19:46:53
 
 ### 문제 설명
 
--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,27 +1,33 @@
 class Solution {
-    public Object solution(int[][] board, int[][] skill) {
-        int answer = 0;
+    public int solution(int[][] board, int[][] skill) {
+        int answer = board.length * board[0].length;
         int[][] damage = new int[board.length + 1][board[0].length + 1];
         
         for ( int[] s : skill ) {
-            int type = ( s[0] == 1 ) ? -1 : 1;
-            damage[s[1]][s[2]] += type * s[5];
-            damage[s[1]][s[4]+1] -= type * s[5];
-            damage[s[3]+1][s[2]] -= type * s[5];
-            damage[s[3]+1][s[4]+1] += type * s[5];
+            int degree = ( s[0] == 1 ) ? -s[5] : s[5];
+            damage[s[1]][s[2]] += degree;
+            damage[s[1]][s[4]+1] -= degree;
+            damage[s[3] + 1][s[2]] -= degree;
+            damage[s[3] + 1][s[4] + 1] += degree;
         }
         
-        for ( int i = 0 ; i < damage.length ; i++ ) 
-            for ( int j = 1 ; j < damage[0].length ; j++ ) 
-                damage[i][j] += damage[i][j-1];
+        for ( int r = 0 ; r <= board.length ; r++ ) {            
+            for ( int c = 1 ; c <= board[0].length ; c++ ) {
+                damage[r][c] += damage[r][c-1];
+            }
+        }
         
-        for ( int i = 0 ; i < damage[0].length ; i++ ) 
-            for ( int j = 1 ; j < damage.length ; j++ ) 
-                damage[j][i] += damage[j-1][i];           
+        for ( int c = 0 ; c <= board[0].length ; c++ ) {            
+            for ( int r = 1 ; r <= board.length ; r++ ) {
+                damage[r][c] += damage[r-1][c];
+            }
+        }        
         
-        for ( int i = 0 ; i < board.length ; i++ ) 
-            for ( int j = 0 ; j < board[0].length ; j++) 
-                if ( board[i][j] + damage[i][j] > 0 ) answer++;
+         for ( int r = 0 ; r < board.length ; r++ ) {            
+            for ( int c = 0 ; c < board[0].length ; c++ ) {
+                if ( board[r][c] + damage[r][c] <= 0 ) answer--;
+            }
+        }       
         
         return answer;
     }
